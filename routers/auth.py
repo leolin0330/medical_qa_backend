769ed970ed01243c910176c58b026be18998c232
leolin0330 <leolin388@gmail.com>
2026-03-16 15:27:43 +0800

    Update auth.py
    20260316

--- routers/auth.py
+++ routers/auth.py
@@ -4,21 +4,53 @@ from jose import JWTError, jwt
 
 from models.user import UserLogin, fake_user_db
 from utils.security import verify_password, create_access_token
+from utils.security import SECRET_KEY, ALGORITHM
+from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from utils.security import hash_password
+
+from sqlalchemy.orm import Session
+from database.db import get_db
+from models.user_model import User
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
+class UserRegister(BaseModel):
+    username: str
+    password: str
+    name:str
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+        return username
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
 
 @router.post("/login")
-def login(user: UserLogin):
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
 
-    db_user = fake_user_db.get(user.username)
+    user = db.query(User).filter(
+        User.username == form_data.username
+    ).first()
 
-    if not db_user:
+    if not user:
         raise HTTPException(status_code=400, detail="User not found")
 
-    if not verify_password(user.password, db_user["password"]):
+    if not verify_password(form_data.password, user.password):
         raise HTTPException(status_code=400, detail="Incorrect password")
 
     access_token = create_access_token(
@@ -31,8 +63,9 @@ def login(user: UserLogin):
     }
 
 
+
 @router.get("/me")
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme),db: Session = Depends(get_db)):
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -44,6 +77,40 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
+    # 這裡要查資料庫
+    user = db.query(User).filter(
+        User.username == username
+    ).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
     return {
-        "username": username
+        "username": user.username,
+        "name": user.name
+    }
+
+
+
+@router.post("/register")
+def register(user: UserRegister, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(
+        User.username == user.username
+    ).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    new_user = User(
+        username=user.username,
+        password=hash_password(user.password),
+        name=user.name
+    )
+
+    db.add(new_user)
+    db.commit()
+
+    return {
+        "message": "User created successfully"
     }
